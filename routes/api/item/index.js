60880aae8f8164ab0136fe4921f52c70c3acb4cc
KorJeongHyeonLee <nomar0107@gmail.com>
2019-01-05 21:48:32 +0900

    아이템 관련 API 위한 디렉토리 생성

---
+++ routes/api/item/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const controller = require('./item.controller');
+
+
+
+module.exports = router;
+
---
+++ routes/api/item/item.controller.js
@@ -0,0 +1,4 @@
+const query = require("../common/query");
+
+
+
