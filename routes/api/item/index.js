7684008f348e05f8b89303593913530ac7c2cef1
KorJeongHyeonLee <nomar0107@gmail.com>
2019-01-18 20:37:47 +0900

    Update index.js

--- routes/api/item/index.js
+++ routes/api/item/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('./item.controller');
 
 router.post('', controller.createItem);
-
+router.post('/size', controller.createSize);
 module.exports = router;
 
