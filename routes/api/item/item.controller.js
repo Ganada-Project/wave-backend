74b4f2c183a4645f054e525779e713bb18b6cfb2
KorJeongHyeonLee <nomar0107@gmail.com>
2019-01-18 20:37:49 +0900

    Update item.controller.js

--- routes/api/item/item.controller.js
+++ routes/api/item/item.controller.js
@@ -9,19 +9,18 @@ exports.createItem = async (req, res) => {
 
         category = await query.category.createCategory(category1,category2,category3);
         feature = await query.feature.createFeature(elasticity,quality,thickness,texture,lining,opacity);
-        size = await query.size.createSize(1,1,1,1,1,1,1,1,"L");
+        // size = await query.size.createSize(1,1,1,1,1,1,1,1,"L");
         category_id = category.insertId;
         feature_id = feature.insertId;
-        size_id = size.insertId;
-        item = await query.item.createItem(brand_id,sex,category_id,style,size_id,feature_id,name,price,season);
+        // size_id = size.insertId;
+        item = await query.item.createItem(brand_id,sex,category_id,style,feature_id,name,price,season);
         item_id = item.insertId;
-        //지우가 할일. 여기서 하나의 아이템이 만들어지고 만들어진 아이템 아이디가 바로위에 있는 변수 item_id 에 저장됨.
-        //Item_Image 테이블 만들어놨음 (시퀄참조)
+
         for(let i = 0; i < images.length; i++) {
             let image_url = await query.image.uploadImage(images[i]);
             let result = await query.item.saveItemImage(item_id, image_url);
         }
-        //req.body 에서 images가 base64배열이라고 생각하고 이미지 각각 서버에 올린다음 Item_Image table 채우기
+
         return res.status(200).json({
             message:"success"
         })
@@ -30,3 +29,16 @@ exports.createItem = async (req, res) => {
     }
 };
 
+exports.createSize = async (req, res) => {
+    const {height,waist,chest,arm,shoulder,thigh,hip,leg,name,item_id} = req.body;
+    try {
+
+        size = await query.size.createSize(height,waist,chest,arm,shoulder,thigh,hip,leg,name,item_id);
+
+        return res.status(200).json({
+            message:"success"
+        })
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+}
