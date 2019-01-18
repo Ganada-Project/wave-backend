d074f7bf6f34812fa38d7ec260603fa5013b6059
KorJeongHyeonLee <nomar0107@gmail.com>
2019-01-18 20:37:37 +0900

    Create query.js

---
+++ routes/api/common/itemstyle/query.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const config = require('../../../../config');
+const conn = mysql.createConnection(config);
+
+exports.getStyleById = (id) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM Style WHERE id = ${id}`, (err, result) => {
+            if (err) reject(err);
+            else resolve(result[0])
+        });
+    });
+}
+
+exports.getAllStyles = () => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM Style`, (err, result) => {
+            if (err) reject(err);
+            else resolve(result)
+        });
+    });
+}
+
+exports.getAllStyles = () => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM Style`, (err, result) => {
+            if (err) reject(err);
+            else resolve(result)
+        });
+    });
+}
+
