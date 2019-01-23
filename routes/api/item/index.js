6f06b767cd85724125cee3793d63d3c3e083b419
Yoon Jee Woo <yoonjeewoo@gmail.com>
2019-01-23 21:48:01 +0900

    (웹) 아이템 아이디로 아이템 정보 가져오기 API

--- routes/api/item/index.js
+++ routes/api/item/index.js
@@ -4,5 +4,10 @@ const controller = require('./item.controller');
 router.post('', controller.createItem);
 router.post('/size', controller.createSize);
 router.get('', controller.getItemsByBrandId);
+router.get('/one', controller.getItemById);
+
+// router.post('/onsale/:item_id', controller.makeOnSale);
+// router.post('/offsale/:item_id', controller.makeOffSale);
+
 module.exports = router;
 
