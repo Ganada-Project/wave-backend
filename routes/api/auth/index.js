3db50303ae11023d5c808202271b4718c381ce2b
KorJeongHyeonLee <nomar0107@gmail.com>
2019-02-14 22:24:29 +0900

    실수

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -5,7 +5,7 @@ router.post('/register', controller.register);
 router.post('/register/brand', controller.register_brand);
 router.get('/check/brand/name', controller.checkBrandNameOverlap);
 router.get('/check/brand/email', controller.checkBrandEmailOverlap);
-router.get('/check/user/phone', controller.checkPhoneNumberOverlap);
+router.post('/check/user/phone', controller.checkPhoneNumberOverlap);
 router.get('/phone', controller.getVerificationSMS);
 router.post('/check/nickname', controller.checkNicknameOverlap);
 router.post('/login/brand', controller.brandLogin);
--- routes/api/brand/index.js
+++ routes/api/brand/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('./brand.controller');
 
-router.post('/recommend', controller.recommendBrandByStyle);
+router.get('/recommend', controller.recommendBrandByStyle);
 
 module.exports = router;
