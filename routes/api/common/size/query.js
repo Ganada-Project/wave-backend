13c422a772ba626f3ee98067d2f8e1ab1e221a2c
JeeWoo Yoon <jeewooyoon@JeeWooui-MacBookPro.local>
2019-08-04 15:55:38 +0900

    size measure

--- routes/api/common/size/query.js
+++ routes/api/common/size/query.js
@@ -2,16 +2,10 @@ const mysql = require('mysql');
 const config = require('../../../../config');
 const conn = mysql.createConnection(config);
 
-// const FCM = require('fcm-node');
-// const serverKey = 'AAAAyS0H1u0:APA91bFX9VjAXOe6hGbGu7CvRQg_qRZzFdOjwY_qper2qVxpiY6P-LEb5KLk_Rh96r0N9iD_NVm6yAwxzIqUZ702_wDQ2RZiNzBS9XdD3Ckf1L_bPxXHERiFmeT58g4REHGPZmT0If8G'; //put your server key here
-// const fcm = new FCM(serverKey);
-// const crypto = require('crypto');
-// const https = require("https");
 
-
-exports.createSize= (conn,card_id,shoulder,chest,arm,waist,height,hip,crotch,thigh,leg) => {
+exports.createSize= (conn,card_id,shoulder,chest,arm,waist,weight,height,hip,crotch,thigh,leg) => {
     return new Promise((resolve, reject) => {
-        conn.query('INSERT INTO Size(card_id,shoulder,chest,arm,waist,height,hip,crotch,thigh,leg)' +
+        conn.query('INSERT INTO Size(card_id,shoulder,chest,weight,arm,waist,height,hip,crotch,thigh,leg)' +
             ' VALUES(?,?,?,?,?,?,?,?,?,?,?)',
             [height,waist,chest,arm,shoulder,thigh,hip,leg,name,item_id,remain],
             (err, result) => {
--- routes/api/common/util/math.js
+++ routes/api/common/util/math.js
@@ -2,36 +2,65 @@ const mysql = require('mysql');
 const config = require('../../../../config');
 const conn = mysql.createConnection(config);
 
+// Height: headOffset.y -  leftFootOffset.y
+// Shoulder: leftShoulderOffset - RightShoulderOffset
+// Chest: leftChestOffset - RightChestOffset
+// Waist: leftWaistOffset - RightWaistOffset
+// Arm: Euclidean(rightShoulderOffset, rightElbowOffset) + Euclidean(rightElbowOffset, rightHandOffset)
+// Hip: leftPelvisOffset - rightPelvisOffset
+// Crotch: pelvisOffset.y - thighOffset.y
+// Thigh: (leftThighOffset - rightThighOffset)/2
+// Leg: Euclidean(leftThighOffset, leftAncleOffset)
 
+// body_points: [
+//     { …headOffset },
+//     { …footOffset },
+//     { …leftNeckOffset },
+//     { …leftShulderOffset },
+//     { …leftElbowOffset },
+//     { …leftHandOffset },
+//     { …rightNeckOffset },
+//     { …rightShulderOffset },
+//     { …rightElbowOffset },
+//     { …rightHandOffset },
+//     { …leftChestOffset },
+//     { …leftWaistOffset },
+//     { …leftPelvisOffset },
+//     { …rightChestOffset },
+//     { …rightWaistOffset },
+//     { …rightPelvisOffset },
+//     { …leftThighOffset },
+//     { …leftAnkleOffset },
+//     { …rightThighOffset },
+//     { …rightAnkleOffset },
+//   ],
 
-exports.calculateDistance = (x_1, y_1, x_2, y_2) => {
-    return (x_1-x_2)*(x_1-x_2)+(y_1-y_2)*(y_1-y_2)
-};
-exports.calculateDistanceVertically = (x_1, y_1, x_2, y_2) => {
-    return y_2-y_1
-};
-exports.calculateDistanceHorizontally = (x_1, y_1, x_2, y_2) => {
-    return x_2-x_1
-};
+function Euclidean(Point1, Point2) {
+    return Math.sqrt((Point1.y-Point2.y)**2 + (Point1.x-Point2.y)**2)    
+}
+// shoulder,chest,arm,waist,height,hip,crotch,thigh,leg
+exports.measureSize = (height, body_points) => {
+    height_ratio = height / (body_points.headOffset.y -  body_points.leftFootOffset.y)
+    shoulder = (body_points.leftShoulderOffset.x - body_points.rightShoulderOffset.x)*height_ratio
+    chest = (body_points.leftChestOffset.x - body_points.RightChestOffset.x)*height_ratio
+    arm = Euclidean(body_points.rightShoulderOffset, body_points.rightElbowOffset) + Euclidean(body_points.rightElbowOffset, body_points.rightHandOffset)
+    waist = body_points.leftWaistOffset.x - body_points.RightWaistOffset.x
+    hip = body_points.leftPelvisOffset.x - body_points.rightPelvisOffset.x
+    crotch = body_points.pelvisOffset.y - body_points.thighOffset.y
+    thigh = (body_points.leftThighOffset.x - body_points.rightThighOffset.x)/2
+    leg = Euclidean(body_points.leftThighOffset, body_points.leftAncleOffset)
+    
+    measure = {
+        shoulder,
+        chest,
+        arm,
+        waist,
+        height,
+        hip,
+        crotch,
+        thigh,
+        leg
+    }
 
-exports.calculateRoundByTwoDistance = (d1,d2) => {
-    return 2*(d1+d2)/0.9
-};
-
-exports.calculateShoulderByCoordinate = (x_1,y_1,x_2,y_2) => {
-    return calculateDistance(x_1,y_1,x_2,y_2);
-};
-
-exports.calculateHeightByCoordinate = (x_1,y_1,x_2,y_2) => {
-    return calculateDistanceVertically(x_1,y_1,x_2,y_2);
-};
-
-exports.calculateArmByCoordinate = (x_1,y_1,x_2,y_2) => {
-    return calculateDistance(x_1,y_1,x_2,y_2);
-};
-
-exports.calculateArmByCoordinate = (x_1,y_1,x_2,y_2) => {
-    return calculateDistance(x_1,y_1,x_2,y_2);
-};
-
-
+    return measure
+}
