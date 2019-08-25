463fc9a66183dd5e4a1e2ea83dda4ebfdc3279e3
JeeWoo Yoon <jeewooyoon@JeeWooui-MacBookPro.local>
2019-08-25 14:14:30 +0900

    manager login

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -158,42 +158,75 @@ exports.brandLogin = (req, res) => {
 }
 
 exports.login = (req, res) => {
-    const { phone, password } = req.body;
+    const { phone, password, is_manager } = req.body;
     // console.log(email,password,config.secret);
     const secret = req.app.get('jwt-secret');
     const encrypted = crypto.createHmac('sha1', config.secret)
         .update(password)
         .digest('base64');
     // console.log(encrypted);
-    conn.query(
-        'SELECT * from User WHERE phone=? and password=?',
-        [phone, encrypted],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length == 0) {
-                return res.status(404).json({ message: 'login failed'});
-            } else {
-                jwt.sign(
-                    {
-                        _id: result[0].id,
-                        phone: result[0].phone
-                    },
-                    secret,
-                    {
-                        expiresIn: '100d',
-                        issuer: 'rebay_admin',
-                        subject: 'userInfo'
-                    }, async(err, token) => {
-                        if (err) return res.status(406).json({ message:'login failed' });
-                        // await query.renewFcmtoken(result[0].id, fcm_token);
-                        return res.status(200).json({
-                            message: 'logged in successfully',
-                            token
+    if (is_manager==1) {
+        conn.query(
+            'SELECT * from User WHERE phone=? and password=? and user_type=1',
+            [phone, encrypted],
+            (err, result) => {
+                if (err) throw err;
+                if (result.length == 0) {
+                    return res.status(404).json({ message: 'login failed'});
+                } else {
+                    jwt.sign(
+                        {
+                            _id: result[0].id,
+                            phone: result[0].phone
+                        },
+                        secret,
+                        {
+                            expiresIn: '100d',
+                            issuer: 'rebay_admin',
+                            subject: 'userInfo'
+                        }, async(err, token) => {
+                            if (err) return res.status(406).json({ message:'login failed' });
+                            // await query.renewFcmtoken(result[0].id, fcm_token);
+                            return res.status(200).json({
+                                message: 'logged in successfully',
+                                token
+                            });
                         });
-                    });
+                }
             }
-        }
-    )
+        )
+    } else {
+        conn.query(
+            'SELECT * from User WHERE phone=? and password=?',
+            [phone, encrypted],
+            (err, result) => {
+                if (err) throw err;
+                if (result.length == 0) {
+                    return res.status(404).json({ message: 'login failed'});
+                } else {
+                    jwt.sign(
+                        {
+                            _id: result[0].id,
+                            phone: result[0].phone
+                        },
+                        secret,
+                        {
+                            expiresIn: '100d',
+                            issuer: 'rebay_admin',
+                            subject: 'userInfo'
+                        }, async(err, token) => {
+                            if (err) return res.status(406).json({ message:'login failed' });
+                            // await query.renewFcmtoken(result[0].id, fcm_token);
+                            return res.status(200).json({
+                                message: 'logged in successfully',
+                                token
+                            });
+                        });
+                }
+            }
+        )
+    }
+    
 };
 
 exports.getUserToken = (req, res) => {
