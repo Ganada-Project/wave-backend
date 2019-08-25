63a13b5ba3b98a19e18176947cf0b6d8283bc9a0
KorJeongHyeonLee <nomar0107@gmail.com>
2019-08-25 16:14:41 +0900

    remove redundant

--- routes/api/common/query.js
+++ routes/api/common/query.js
@@ -1,41 +1,30 @@
 
-const body = require('../common/body/query');
+
 const image = require('../common/image/query');
 const personal = require('../common/personal/query');
 const sms = require('../common/sms/query');
 const style = require('../common/style/query');
 const user = require('../common/user/query');
 const userstyle = require('../common/userstyle/query');
-const brand = require('../common/brand/query');
-const brandstyle = require('../common/brandstyle/query');
-const category = require('../common/category/query');
-const feature = require('../common/feature/query');
 const size = require('../common/size/query');
 const item = require('../common/item/query');
 const itemsize = require('../common/itemsize/query');
 const sizemeasure = require('../common/sizemeasure/query');
 const season = require('../common/season/query');
-const brandfollow = require('../common/brandfollow/query');
 const card = require('../common/card/query');
 const util = require('../common/util/math');
 module.exports = {
-    body,
     image,
     personal,
     sms,
     style,
     user,
     userstyle,
-    brand,
-    brandstyle,
-    category,
-    feature,
     item,
     size,
     itemsize,
     sizemeasure,
     season,
-    brandfollow,
     util,
     card
 };
